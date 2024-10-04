9fc63b2b7d799c28a4355c23ce98c35e20708112
goingobsolete <goingobsolete@gmail.com>
2024-10-04 17:23:17 +0200

    added structure to assets folder to mirror data.js format

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const jsons = {
+    grp1: null,
+    grp2: null,
+    grp3: null,
+    grp4: null,
+    grp5: null,
+    wordArray: null,
+};
+
+export const fonts = {
+    textFont: null,
+    signFont: null,
+};
+
+export const images = {
+    bgTile: null,
+};
+
+export const preload = (p) => {
