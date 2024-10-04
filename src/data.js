9647f9f54236d7e8f6a6b158f5392ee737796e46
goingobsolete <goingobsolete@gmail.com>
2024-10-04 18:03:28 +0200

    finished 1st pass data.js

--- src/data.js
+++ src/data.js
@@ -12,8 +12,22 @@ export const fonts = {
     signFont: null,
 };
 
-export const images = {
-    bgTile: null,
-};
+// export const images = {
+//     bgTile: null,
+// };
 
-export const preload = (p) => {
+// export const sounds = {
+//     exSound: null,
+// };
+
+export const preload = (pre) => {
+    jsons.grp1 = pre.json('grp1', 'assets/jsons/group1.json');
+    jsons.grp2 = pre.json('grp2', 'assets/jsons/group2.json');
+    jsons.grp3 = pre.json('grp3', 'assets/jsons/group3.json');
+    jsons.grp4 = pre.json('grp4', 'assets/jsons/group4.json');
+    jsons.grp5 = pre.json('grp5', 'assets/jsons/group5.json');
+    jsons.wordArray = pre.json('wordArray', 'assets/jsons/words_with_line_numbers.json');
+
+    fonts.textFont = pre.loadFont('assets/fonts/Libre_Caslon_Text/LibreCaslonText-Regular.ttf');
+    fonts.signFont = pre.loadFont('assets/fonts/Libre_Caslon_Display/LibreCaslonDisplay-Regular.ttf');
+};
