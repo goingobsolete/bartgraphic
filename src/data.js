9719020946221e8a74c45cd0d66c24a5616e06e7
goingobsolete <goingobsolete@gmail.com>
2024-10-09 12:52:29 +0200

    commented out jsons for now - not sure if running

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
-export const jsons = {
-    grp1: null,
-    grp2: null,
-    grp3: null,
-    grp4: null,
-    grp5: null,
-    wordArray: null,
-};
+// export const jsons = {
+//     grp1: null,
+//     grp2: null,
+//     grp3: null,
+//     grp4: null,
+//     grp5: null,
+//     wordArray: null,
+// };
 
 export const fonts = {
     textFont: null,
@@ -21,12 +21,12 @@ export const fonts = {
 // };
 
 export const preload = (pre) => {
-    jsons.grp1 = pre.json('grp1', 'assets/jsons/group1.json');
-    jsons.grp2 = pre.json('grp2', 'assets/jsons/group2.json');
-    jsons.grp3 = pre.json('grp3', 'assets/jsons/group3.json');
-    jsons.grp4 = pre.json('grp4', 'assets/jsons/group4.json');
-    jsons.grp5 = pre.json('grp5', 'assets/jsons/group5.json');
-    jsons.wordArray = pre.json('wordArray', 'assets/jsons/words_with_line_numbers.json');
+    // jsons.grp1 = pre.json('grp1', 'assets/jsons/group1.json');
+    // jsons.grp2 = pre.json('grp2', 'assets/jsons/group2.json');
+    // jsons.grp3 = pre.json('grp3', 'assets/jsons/group3.json');
+    // jsons.grp4 = pre.json('grp4', 'assets/jsons/group4.json');
+    // jsons.grp5 = pre.json('grp5', 'assets/jsons/group5.json');
+    // jsons.wordArray = pre.json('wordArray', 'assets/jsons/words_with_line_numbers.json');
 
     fonts.textFont = pre.loadFont('assets/fonts/Libre_Caslon_Text/LibreCaslonText-Regular.ttf');
     fonts.signFont = pre.loadFont('assets/fonts/Libre_Caslon_Display/LibreCaslonDisplay-Regular.ttf');
